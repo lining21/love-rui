e954850f15b539ff8d1ab8a3be65aa7ac6897d39
lining <wu515@tuya.com>
2025-03-20 14:45:37 +0800

    feat: 调整打包路径

--- code/vue.config.js
+++ code/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // publicPath: '/gift-for-wedding/', // 打包路径
+  publicPath: './', // 打包路径
   transpileDependencies: true
 })
